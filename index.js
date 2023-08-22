1164661e177bd92ec04802cbc81aa1d5931c0395
F-zeinali <fatemeh.zeinali7026@gmail.com>
2023-08-23 00:06:42 +0430

    create login-modal

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const date = new Date().toLocaleString();
 document.getElementById("date").innerHTML = date;
+
+
+let modal = document.getElementById("loginModal");
+let btn = document.getElementById("loginBtn");
+btn.addEventListener("click", () => modal.style.display = "block" );
+
+
+let close = document.getElementsByClassName("close");
+
