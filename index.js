4d4573e6a0c5903ba622a92638934a94ed805900
F-zeinali <fatemeh.zeinali7026@gmail.com>
2023-08-23 16:21:19 +0430

    create login-modal

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ document.getElementById("date").innerHTML = date;
 
 
 let modal = document.getElementById("loginModal");
+window.addEventListener("click", (event) => {if (event.target == modal) {
+    modal.style.display = "none";
+}})
+
 let btn = document.getElementById("loginBtn");
 btn.addEventListener("click", () => modal.style.display = "block" );
 
-
-let close = document.getElementsByClassName("close");
+let close = document.getElementsByClassName("close")[0];
+close.addEventListener("click", () => modal.style.display="none" );
 
