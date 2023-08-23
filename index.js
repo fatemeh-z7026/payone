fea1a6148b3268fde22c88ded88ca9c3b745782c
F-zeinali <fatemeh.zeinali7026@gmail.com>
2023-08-23 23:26:45 +0430

    create login-modal

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ btn.addEventListener("click", () => modal.style.display = "block" );
 let close = document.getElementsByClassName("close")[0];
 close.addEventListener("click", () => modal.style.display="none" );
 
+
+
+function switchCard() {
+    const loginCard =document.querySelector(".modalContent #cardLogin");
+    const registerCard =document.querySelector(".modalContent #cardRegister");  
+
+    if (loginCard.style.display === 'none') {
+        loginCard.style.display = 'block';
+        registerCard.style.display = 'none';
+      } else {
+        loginCard.style.display = 'none';
+        registerCard.style.display = 'block';
+      }
+}
