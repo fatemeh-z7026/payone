a574b4d97436b451df893b32269620cf5dc42820
F-zeinali <fatemeh.zeinali7026@gmail.com>
2023-08-30 10:47:59 +0430

    create success alert

--- index.js
+++ index.js
@@ -4,54 +4,57 @@ document.getElementById("date").innerHTML = date;
 
 //Modal
 let modal = document.getElementById("loginModal");
-window.addEventListener("click", (event) => {if (event.target == modal) {
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
     modal.style.display = "none";
-}})
+  }
+});
 
 let btn = document.getElementById("loginEntryBtn");
-btn.addEventListener("click", () => modal.style.display = "block" );
+btn.addEventListener("click", () => (modal.style.display = "block"));
 
 let close = document.getElementsByClassName("close")[0];
-close.addEventListener("click", () => modal.style.display="none" );
-
+close.addEventListener("click", () => (modal.style.display = "none"));
 
 //Switch Btw Login and Register Card
 function switchCard() {
-    const loginCard =document.querySelector(".modalContent #cardLogin");
-    const registerCard =document.querySelector(".modalContent #cardRegister");  
+  const loginCard = document.querySelector(".modalContent #cardLogin");
+  const registerCard = document.querySelector(".modalContent #cardRegister");
 
-    if (loginCard.style.display === 'none') {
-        loginCard.style.display = 'block';
-        registerCard.style.display = 'none';
-      } else {
-        loginCard.style.display = 'none';
-        registerCard.style.display = 'block';
-      }
+  if (loginCard.style.display === "none") {
+    loginCard.style.display = "block";
+    registerCard.style.display = "none";
+  } else {
+    loginCard.style.display = "none";
+    registerCard.style.display = "block";
+  }
 }
-
-
 
 //Success Alert
 const closeAlert = document.getElementsByClassName("closebtn");
 
-
 for (i = 0; i < closeAlert.length; i++) {
-  closeAlert[0].onclick = function(){
+  closeAlert[0].onclick = function () {
     const div = this.parentElement;
-    div.style.opacity = "0";
-     div.style.display = "none";;
-  }
+    // div.style.opacity = "0";
+    div.style.display = "none";
+  };
 }
 
 //Submit Login
-const submitLogin = document.getElementById("submit");
-submitLogin.addEventListener("click", function() {
-  const success = document.getElementById("alertSuccess");
-  
-  success.style.display = "block";
-  modal.style.display = "none";
-  
-})
+const submitLogin = document.querySelectorAll("button[type = submit]");
+submitLogin.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const success = document.getElementById("alertSuccess");
+    if ((success.style.display = "none")) {
+      success.style.display = "block";
+      modal.style.display = "none";
+      setTimeout(() => {
+        success.style.display = "none";
+      }, 3000);
+    }
+  });
+});
 
 //NavBar Toggle
 
